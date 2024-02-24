4dc6f44f28b5ae2d1cf3bfd390b25c99c8706b4b
byeongseok <kbs970318@gmail.com>
2024-02-24 21:59:30 +0900

    증감연산자 전위, 후위

---
+++ 연산자/증감연산자 전위, 후위/IncDecOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncDecOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++); // a = a + 1
+                                    // (a++ 순서처럼 a가 10이니 10 먼저 출력, 그 후에 1을 증가 시켜 값은 11이 출력되서 나옴)
+                                    // 2씩 증가 하고 싶으면 a = a + 2 ...
+            Console.WriteLine(++a); //  (++a 편하게 순서대로 생각하면 됨 1이 먼저 출력되고 10이 더해지는... 후위증가 연산자)
+
+            Console.WriteLine(a--); // a = a - 1 (+ 와 반대 개념)
+            Console.WriteLine(--a); // + 와 반대
+        }
+    }
+}
