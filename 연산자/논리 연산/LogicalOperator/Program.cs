17c75f7a6b8690c328b205f74f49fe99f171a91e
byeongseok <kbs970318@gmail.com>
2024-02-24 21:48:42 +0900

    논리 연산자 &&그리고 ||또는

---
+++ 연산자/논리 연산/LogicalOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing && ...");
+            Console.WriteLine("1 > 0 && 4 < 5 : {0}", 1 > 0 && 4 < 5); ;
+            Console.WriteLine("1 > 0 && 4 > 5 : {0}", 1 > 0 && 4 > 5); ;
+            Console.WriteLine("1 == 0 && 4 > 5 : {0}", 1 == 0 && 4 > 5); ;
+            Console.WriteLine("1 == 0 && 4 < 5 : {0}", 1 == 0 && 4 < 5); ;
+
+            Console.WriteLine("\nTesting || ...");
+            Console.WriteLine("1 > 0 || 4 < 5 : {0}", 1 > 0 || 4< 5);
+            Console.WriteLine("1 > 0 || 4 > 5 : {0}", 1 > 0 || 4 > 5);
+            Console.WriteLine("1 == 0 || 4 > 5 : {0}", 1 == 0 || 4 > 5);
+            Console.WriteLine("1 == 0 || 4 < 5 : {0}", 1 == 0 || 4 < 5);
+
+            Console.WriteLine("\nTesting ! ...");
+            Console.WriteLine("!true : {0}", !true);
+            Console.WriteLine("!false : {0}", !false);
+        }
+    }
+}
